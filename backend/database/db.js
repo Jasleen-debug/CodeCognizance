002f7d04f97e5c9cca947d678e82814e5a013605
Jasleen <jasleen0859@gmail.com>
2024-07-15 03:58:47 -0500

    Created a user model for MongoDB user collection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,1 +1,2 @@
 // Database connection configuration
+import mongoose from "mongoose";
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,2 +1,23 @@
-//User schema or model
 import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
