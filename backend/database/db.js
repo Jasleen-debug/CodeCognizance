358c43d449c868c31d84cc0c3709b928b991e717
Jasleen <jasleen0859@gmail.com>
2024-07-15 19:47:17 -0500

    Connected application to MongoDB and created a DB Connection file

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,2 +1,20 @@
 // Database connection configuration
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+import path from "path"
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
+
+const DBConnection = async () => {
+  try {
+    const MONGO_URI = process.env.MONGO_URI
+
+    await mongoose.connect(MONGO_URI)
+    console.log('MongoDB connected successfully')
+  } catch (error) {
+    console.log('Error while connecting to MongoDB', error.message);
+    process.exit(1)
+  }
+}
+
+export default DBConnection
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 //const express = require('express') - this is old way of importing ok
 import express from 'express' // you need to go to package.json and add type = module
-import authRoutes from './routes/authRoutes'
+import dotenv from 'dotenv'
+import { authRouter } from './routes/authRoutes.js'
+import DBConnection from './database/db.js';
+dotenv.config()
 
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-app.use('/auth',authRoutes)
+app.use('/auth', authRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+
+
+// Connect to the database and start the server
+DBConnection().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+}).catch((error) => {
+    console.error('Failed to connect to the database:', error)
 })
+
+
 
