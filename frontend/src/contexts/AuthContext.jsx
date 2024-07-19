6f149ee3a9dca63d033c42c1d03b2bc45559d70f
Jasleen <jasleen0859@gmail.com>
2024-07-18 23:49:53 -0500

    corrected the export statement for AuthProvider

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import {login as loginService, logout as logoutService, checkAuth as checkAuthSe
 
 export const AuthContext = createContext()
 
-const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     isAuthenticated: false,
     loading: true,
@@ -69,5 +69,3 @@ const AuthProvider = ({ children }) => {
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired
 }
-
-export default AuthProvider
