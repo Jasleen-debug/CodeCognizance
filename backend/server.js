6f6c9973b4f62a2fb599580c3040702e4a92fe0f
Jasleen <jasleen0859@gmail.com>
2024-08-07 08:33:55 -0500

    corrected errors in server entrypoint file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ import { runRouter } from './routes/run.js'
 import DBConnection from './database/db.js'
 import cookieParser from 'cookie-parser'
 import { judgeRouter } from './routes/judge.js'
-import {submissionRouter} from './routes/submissions.js'
+import { submissionRouter } from './routes/submissions.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 dotenv.config()
 
 const app = express()
@@ -18,10 +21,10 @@ const ORIGIN = process.env.ORIGIN
 const corsOptions = {
   origin: ORIGIN || 'http://localhost:5173',
   credentials: true,
-};
+}
 
 // Apply CORS middleware
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 // Parse JSON bodies
 app.use(express.json())
 // Parse URL-encoded bodies
@@ -38,6 +41,8 @@ app.get('/', (req,res) => {
   res.send('Hello world')
 })
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 // Serve static files from the Vite app's dist directory
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
